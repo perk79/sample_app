75b36fb49600a1556b33ad53d3777be4daf1afb8
Per Kauffeldt <perk79@me.com>
2011-11-25 19:04:42 +0100

    Done with sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,13 @@ class User < ActiveRecord::Base
     return user if user.has_password?(submitted_password)
   end
   
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
+    # Equal to: return nil if user.nil?
+    #           return user if user.salt == cookie_salt
+  end
+  
   private
 
     def encrypt_password
